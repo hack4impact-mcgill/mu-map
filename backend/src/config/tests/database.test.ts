63b9a468d5db4e566a07ce7f03d750ee93542139
JDziewonski98 <JDziewonski98@users.noreply.github.com>
2020-11-09 03:37:23 +0000

    Prettified Code!

--- backend/src/config/tests/database.test.ts
+++ backend/src/config/tests/database.test.ts
@@ -1,38 +1,36 @@
-import {database} from "../database"
-import {Mural} from "../../models/mural.model"
+import { database } from "../database";
+import { Mural } from "../../models/mural.model";
 
 beforeAll(async () => {
-    //TODO need serious help how to eliminate errors from this line...
-    //could it be due to outdated postgres version?
-    await database.sync();
-})
+  //TODO need serious help how to eliminate errors from this line...
+  //could it be due to outdated postgres version?
+  await database.sync();
+});
 
-test('create mural', async () => {
-    expect.assertions(1)
-    const params = {
-            name : "testmural",
-            artist : "bobby",
-            year : 1234,
-            city : "montreal",
-            address : "1234 street",
-            partners : [
-                "partner 1",
-                "partner 2"
-            ]
-    }
-    const mural = await Mural.create<Mural>(params)
-        .catch((err: Error) => fail("Creating mural failed."))
-    expect(mural.id).toEqual(1)
-})
+test("create mural", async () => {
+  expect.assertions(1);
+  const params = {
+    name: "testmural",
+    artist: "bobby",
+    year: 1234,
+    city: "montreal",
+    address: "1234 street",
+    partners: ["partner 1", "partner 2"],
+  };
+  const mural = await Mural.create<Mural>(params).catch((err: Error) =>
+    fail("Creating mural failed.")
+  );
+  expect(mural.id).toEqual(1);
+});
 
-test('get mural', async () => {
-    expect.assertions(3)
-    const mural = await Mural.findByPk(1)
-    expect(mural).not.toEqual(null)
-    expect(mural!.id).toEqual(1)
-    expect(mural!.name).toEqual("testmural")
-})
+test("get mural", async () => {
+  expect.assertions(3);
+  const mural = await Mural.findByPk(1);
+  expect(mural).not.toEqual(null);
+  expect(mural!.id).toEqual(1);
+  expect(mural!.name).toEqual("testmural");
+});
 
 afterAll(async () => {
-    await database.close()
-})
+  await database.close();
+});
