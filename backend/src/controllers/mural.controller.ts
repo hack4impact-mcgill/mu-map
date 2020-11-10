3b893ff970de18bfaa677c0cb20faa655bf2d8c5
JD908 <46234488+JD908@users.noreply.github.com>
2020-11-10 15:58:02 -0800

    refactored to controller and service layer

--- backend/src/controllers/mural.controller.ts
+++ backend/src/controllers/mural.controller.ts
@@ -1,52 +1,50 @@
 import { Request, Response } from "express";
-import { Mural, MuralInterface } from "../models/mural.model";
-import { UpdateOptions } from "sequelize";
+import { MuralInterface} from "../models/mural.model";
+import { EmptyResultError } from "sequelize";
+import { MuralService } from "../services/mural.service"
 
 export class MuralController {
-  // GET /mural
-  public index(req: Request, res: Response) {
-    res.json({
-      message: "Hello from the mural controller.",
-    });
-  }
+
+  public muralService : MuralService = new MuralService();
 
   // POST /mural
-  public create(req: Request, res: Response) {
+  public async create(req: Request, res: Response) {
     const params: MuralInterface = req.body;
-
-    Mural.create<Mural>(params)
-      .then((mural: Mural) => res.status(201).json(mural))
-      .catch((err: Error) => res.status(500).json(err));
+     try {
+      const createdMural = await this.muralService.create(params)
+      res.status(201).json(createdMural)
+    } catch (e) {
+      res.status(500).json(e)
+    }
   }
 
   // GET /mural/:id     returns mural by id
-  public show(req: Request, res: Response) {
+  public async show(req: Request, res: Response) {
     const muralId: number = Number(req.params.id);
 
-    Mural.findByPk<Mural>(muralId)
-      .then((mural: Mural | null) => {
-        if (mural) {
-          res.json(mural);
-        } else {
-          res.status(404).json({ errors: ["Mural not found by id!"] });
-        }
-      })
-      .catch((err: Error) => res.status(500).json(err));
+    try {
+      const mural = await this.muralService.show(muralId)
+      res.status(202).json(mural)
+    } catch (e) {
+      if (e instanceof EmptyResultError) {
+        res.status(404).json(e)
+      }
+      res.status(500).json(e)
+    }
   }
 
   // PUT /mural/:id   update properties of a mural by id
-  public update(req: Request, res: Response) {
+  public async update(req: Request, res: Response) {
     const muralId: number = Number(req.params.id);
     const params: MuralInterface = req.body;
-
-    const update: UpdateOptions = {
-      where: { id: muralId },
-    };
-
-    //TODO first check if that id exists
-
-    Mural.update(params, update)
-      .then(() => res.status(202).json({ data: "successfully updated" }))
-      .catch((err: Error) => res.status(500).json(err));
+    try {
+      await this.muralService.update(muralId, params)
+      res.status(202).json({ data: "successfully updated" });
+    } catch (e) {
+      if (e instanceof EmptyResultError) {
+        res.status(404).json(e)
+      }
+      res.status(500).json(e)
+    }
   }
 }
