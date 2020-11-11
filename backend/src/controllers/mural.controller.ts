719b352fff50994f7ba410d12b1b22b29b63f852
JDziewonski98 <JDziewonski98@users.noreply.github.com>
2020-11-11 00:02:29 +0000

    Prettified Code!

--- backend/src/controllers/mural.controller.ts
+++ backend/src/controllers/mural.controller.ts
@@ -1,20 +1,19 @@
 import { Request, Response } from "express";
-import { MuralInterface} from "../models/mural.model";
+import { MuralInterface } from "../models/mural.model";
 import { EmptyResultError } from "sequelize";
-import { MuralService } from "../services/mural.service"
+import { MuralService } from "../services/mural.service";
 
 export class MuralController {
-
-  public muralService : MuralService = new MuralService();
+  public muralService: MuralService = new MuralService();
 
   // POST /mural
   public async create(req: Request, res: Response) {
     const params: MuralInterface = req.body;
-     try {
-      const createdMural = await this.muralService.create(params)
-      res.status(201).json(createdMural)
+    try {
+      const createdMural = await this.muralService.create(params);
+      res.status(201).json(createdMural);
     } catch (e) {
-      res.status(500).json(e)
+      res.status(500).json(e);
     }
   }
 
@@ -23,13 +22,13 @@ export class MuralController {
     const muralId: number = Number(req.params.id);
 
     try {
-      const mural = await this.muralService.show(muralId)
-      res.status(202).json(mural)
+      const mural = await this.muralService.show(muralId);
+      res.status(202).json(mural);
     } catch (e) {
       if (e instanceof EmptyResultError) {
-        res.status(404).json(e)
+        res.status(404).json(e);
       }
-      res.status(500).json(e)
+      res.status(500).json(e);
     }
   }
 
@@ -38,13 +37,13 @@ export class MuralController {
     const muralId: number = Number(req.params.id);
     const params: MuralInterface = req.body;
     try {
-      await this.muralService.update(muralId, params)
+      await this.muralService.update(muralId, params);
       res.status(202).json({ data: "successfully updated" });
     } catch (e) {
       if (e instanceof EmptyResultError) {
-        res.status(404).json(e)
+        res.status(404).json(e);
       }
-      res.status(500).json(e)
+      res.status(500).json(e);
     }
   }
 }
