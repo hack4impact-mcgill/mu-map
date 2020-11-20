9eb2d52ff95974166739c21a388fe0ab4928eb50
Ted Spare <ted.spare@gmail.com>
2020-11-20 18:30:46 -0500

    Add React PWA template. Add basic assets.

---
+++ frontend/src/App/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Map from '../Map';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App/App.tsx</code> and save to reload.
+        </p>
+      </header>
+      <Map></Map>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Map.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Map.css';
+
+function Map() {
+    return (
+        <div>
+            map
+        </div>
+    );
+}
+
+export default Map;
