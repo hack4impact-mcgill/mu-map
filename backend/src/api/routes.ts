8e8a95b701480ec6e22e11b0c8906b8cdc56f6be
zhenglinxiao <lx.zheng19@outlook.com>
2020-11-21 20:38:45 -0500

    feat: create api to create and get boroughs

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -1,8 +1,10 @@
 import { MuralController } from "../controllers/mural.controller";
+import { BoroughController } from "../controllers/borough.controller";
 import { Application } from "express";
 
 export class Routes {
   public muralController: MuralController = new MuralController();
+  public boroughController: BoroughController = new BoroughController();
 
   public routes(app: Application): void {
     app
@@ -13,5 +15,14 @@ export class Routes {
       .route("/mural/:id")
       .get(this.muralController.show.bind(this.muralController))
       .put(this.muralController.update.bind(this.muralController));
+
+    app
+      .route("/borough")
+      .post(this.boroughController.create.bind(this.boroughController));
+
+    app
+      .route("/borough/:id")
+      .get(this.boroughController.show.bind(this.boroughController))
+      .put(this.boroughController.update.bind(this.boroughController));
   }
 }
