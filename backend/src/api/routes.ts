b8e766c91efdb36dbe8696d102bdfc1c94b2e4dd
Ted Spare <ted.spare@gmail.com>
2020-11-23 15:19:47 -0500

    Resolve merge conflicts

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -1,9 +1,13 @@
 import { MuralController } from "../controllers/mural.controller";
+import { BoroughController } from "../controllers/borough.controller";
+import { ArtistController } from "../controllers/artist.controller";
+import { TourController } from "../controllers/tour.controller";
 import { Application } from "express";
-import {TourController} from "../controllers/tour.controller"
 
 export class Routes {
   public muralController: MuralController = new MuralController();
+  public boroughController: BoroughController = new BoroughController();
+  public artistController: ArtistController = new ArtistController();
   public tourController: TourController = new TourController();
 
   public routes(app: Application): void {
@@ -15,7 +19,29 @@ export class Routes {
       .route("/mural/:id")
       .get(this.muralController.show.bind(this.muralController))
       .put(this.muralController.update.bind(this.muralController));
-    
+
+    app
+      .route("/borough")
+      .post(this.boroughController.create.bind(this.boroughController));
+
+    app
+      .route("/borough/:id")
+      .get(this.boroughController.show.bind(this.boroughController))
+      .put(this.boroughController.update.bind(this.boroughController));
+
+    app
+      .route("/artist")
+      .post(this.artistController.create.bind(this.artistController));
+
+    app
+      .route("/artist/:id")
+      .get(this.artistController.show.bind(this.artistController))
+      .put(this.artistController.update.bind(this.artistController));
+
+    app
+      .route("/tour")
+      .post(this.tourController.create.bind(this.tourController));
+
     app
       .route("/tour/:id")
       .get(this.tourController.show.bind(this.tourController))
--- backend/src/services/tour.service.ts
+++ backend/src/services/tour.service.ts
@@ -4,28 +4,28 @@ import { UpdateOptions } from "sequelize";
 export class TourService {
     public async create(tour: TourInterface) {
         const createdTour = await Tour.create<Tour>(tour);
-        return {success: true, body:createdTour};
+        return { success: true, body: createdTour };
     }
     public async show(tourId: number) {
         const tour = await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
-        return { success: true, tour:tour };
-    }
-    
-    public async update(tourId: number, params:TourInterface) {
-        const update: UpdateOptions = {
-            where: {id:tourId},
-        };
-        await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
-        await Tour.update(params,update);
-        return {success:true};
+        return { success: true, tour: tour };
     }
 
-    public async delete(tourId:number, params:TourInterface) {
-        //const tour = await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
+    public async update(tourId: number, params: TourInterface) {
+        const update: UpdateOptions = {
+            where: { id: tourId },
+        };
+        await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
+        await Tour.update(params, update);
+        return { success: true };
+    }
+
+    public async delete(tourId: number, params: TourInterface) {
+        await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
         await Tour.destroy({
-            where: {id:tourId}
-          });
-        return {success: true};
+            where: { id: tourId }
+        });
+        return { success: true };
     }
 }
 
