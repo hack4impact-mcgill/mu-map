3981498887f3a54d6c41c9a206412ae7f7cf8e05
Blanche francheterre <blanche.francheterre@mail.mcgill.ca>
2020-11-23 14:53:47 -0500

    added tour.service and tour.controller with create,show,update,delete and added route tour/:id

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -1,8 +1,10 @@
 import { MuralController } from "../controllers/mural.controller";
 import { Application } from "express";
+import {TourController} from "../controllers/tour.controller"
 
 export class Routes {
   public muralController: MuralController = new MuralController();
+  public tourController: TourController = new TourController();
 
   public routes(app: Application): void {
     app
@@ -13,5 +15,10 @@ export class Routes {
       .route("/mural/:id")
       .get(this.muralController.show.bind(this.muralController))
       .put(this.muralController.update.bind(this.muralController));
+    
+    app
+      .route("/tour/:id")
+      .get(this.tourController.show.bind(this.tourController))
+      .put(this.tourController.update.bind(this.tourController));
   }
 }
---
+++ backend/src/services/tour.service.ts
@@ -0,0 +1,31 @@
+import { Tour, TourInterface } from "../models/tour.model";
+import { UpdateOptions } from "sequelize";
+
+export class TourService {
+    public async create(tour: TourInterface) {
+        const createdTour = await Tour.create<Tour>(tour);
+        return {success: true, body:createdTour};
+    }
+    public async show(tourId: number) {
+        const tour = await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
+        return { success: true, tour:tour };
+    }
+    
+    public async update(tourId: number, params:TourInterface) {
+        const update: UpdateOptions = {
+            where: {id:tourId},
+        };
+        await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
+        await Tour.update(params,update);
+        return {success:true};
+    }
+
+    public async delete(tourId:number, params:TourInterface) {
+        //const tour = await Tour.findByPk<Tour>(tourId, { rejectOnEmpty: true });
+        await Tour.destroy({
+            where: {id:tourId}
+          });
+        return {success: true};
+    }
+}
+
