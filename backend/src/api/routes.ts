7806e3dd328f47dbbfb5c2860727c75479c39a2e
Tony <36376980+Tony9984@users.noreply.github.com>
2020-11-22 22:04:24 -0500

    feat: implement service & controller methods to create a tour (#24)

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -1,8 +1,10 @@
 import { MuralController } from "../controllers/mural.controller";
+import { TourController } from "../controllers/tour.controller";
 import { Application } from "express";
 
 export class Routes {
   public muralController: MuralController = new MuralController();
+  public tourController: TourController = new TourController();
 
   public routes(app: Application): void {
     app
@@ -13,5 +15,9 @@ export class Routes {
       .route("/mural/:id")
       .get(this.muralController.show.bind(this.muralController))
       .put(this.muralController.update.bind(this.muralController));
+
+    app
+      .route("/route")
+      .post(this.tourController.create.bind(this.tourController));
   }
 }
