ff37dff07bb91320e0799644cfe4411ab5fa47c9
zhenglinxiao <lx.zheng19@outlook.com>
2020-11-29 12:32:45 -0500

    feat: unit tests for artist service

---
+++ backend/src/services/tests/artist.test.ts
@@ -0,0 +1,93 @@
+import { database } from "../../config/database";
+import { Artist } from "../../models/artist.model";
+import { ArtistService } from "../artist.service";
+
+// Not sure how to use jest.mock
+// create() does not accept params if name is missing so test for invalid params is omitted
+
+beforeEach(async () => {
+  await Artist.sync({ force: true });
+});
+
+test("create artist", async () => {
+  expect.assertions(3);
+  const artistService: ArtistService = new ArtistService();
+  const params = {
+    name: "testartist",
+  };
+  const create = await artistService.create(params);
+  expect(create.success).toEqual(true);
+  expect(create.body!.id).toEqual(1);
+  expect(create.body!.name).toEqual("testartist");
+});
+
+test("show valid artist", async () => {
+  expect.assertions(3);
+  const artistService: ArtistService = new ArtistService();
+  const params = {
+    name: "testartist",
+  };
+  // not sure if I should be using this or Artist.create to do this
+  const create = await artistService.create(params);
+  if (!create.success) {
+    fail("Creating artist failed.");
+  }
+  const artist = create.body!;
+
+  const show = await artistService.show(artist.id);
+  expect(show.success).toEqual(true);
+  expect(show.artist!.id).toEqual(artist.id);
+  expect(show.artist!.name).toEqual(artist.name);
+});
+
+test("show invalid artist", async () => {
+  expect.assertions(1);
+  const artistService: ArtistService = new ArtistService();
+  await artistService
+    .show(1)
+    .then(() => fail())
+    .catch((err: Error) => expect(true).toEqual(true));
+});
+
+test("update artist", async () => {
+  expect.assertions(3);
+  const artistService: ArtistService = new ArtistService();
+  const params = {
+    name: "testartist",
+  };
+  const create = await artistService.create(params);
+  if (!create.success) {
+    fail("Creating artist failed.");
+  }
+  const artist = create.body!;
+
+  const newparams = {
+    name: "newartist",
+  };
+
+  const update = await artistService.update(artist.id, newparams);
+  expect(update.success).toEqual(true);
+
+  const show = await artistService
+    .show(artist.id)
+    .catch((err: Error) => fail("Could not find this artist by ID."));
+  const updatedArtist = show.artist!;
+  expect(updatedArtist.id).toEqual(artist.id);
+  expect(updatedArtist.name).toEqual("newartist");
+});
+
+test("update invalid artist", async () => {
+  expect.assertions(1);
+  const artistService: ArtistService = new ArtistService();
+  const params = {
+    name: "newartist",
+  };
+  await artistService
+    .update(1, params)
+    .then(() => fail())
+    .catch((err: Error) => expect(true).toEqual(true));
+});
+
+afterAll(async () => {
+  await database.close();
+});
