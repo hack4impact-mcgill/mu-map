72620bfc71c2a04e55a2ec4cb8598762d57932d6
yi-fang-yuan <yi.f.yuan@mail.mcgill.ca>
2020-12-06 18:17:03 -0500

    Add borough unit testing (#42)
    * feat: implement service & controller methods to create a tour
    
    * get tours
    
    * test for service
    
    * added better error checking for
    malformed create params
    
    Co-authored-by: Tony <tonyou1999@hotmail.com>
    Co-authored-by: Jacek Dziewonski <jacek@live.ca>

---
+++ backend/src/services/tests/borough.test.ts
@@ -0,0 +1,91 @@
+import { database } from "../../config/database";
+import { Borough } from "../../models/borough.model";
+import { BoroughService } from "../borough.service";
+
+
+beforeEach(async () => {
+    await Borough.sync({ force: true });
+  });
+
+test("create borough", async () => {
+    expect.assertions(3);
+    const boroughService: BoroughService = new BoroughService();
+    const params = {
+        name: "testBorough",
+    }
+    const create = await boroughService.create(params)
+    expect(create.success).toEqual(true);
+    expect(create.body.id).toEqual(1);
+    expect(create.body.name).toEqual("testBorough")
+})
+
+
+test("show valid borough", async () =>{
+    expect.assertions(3);
+    const boroughService: BoroughService = new BoroughService();
+    const params = {
+        name: "testBorough",
+    }
+    const create = await boroughService.create(params);
+    if (!create.success)
+    {
+        fail("Creating borough failed.")
+    }
+    const show = await boroughService.show(create.body.id)
+    expect(show.success).toEqual(true);
+    expect(show.borough.id).toEqual(create.body.id);
+    expect(show.borough.name).toEqual(create.body.name);
+})
+
+
+test("Show invalid borough", async() => {
+    expect.assertions(1);
+    const boroughService: BoroughService = new BoroughService();
+    await boroughService
+    .show(1)
+    .then(() => fail())
+    .catch((err: Error) => expect(true).toEqual(true))
+})
+
+test("update borough", async() => {
+    expect.assertions(3)
+    const boroughService: BoroughService = new BoroughService();
+    const params = {
+        name: "testBorough",
+    }
+
+    const create = await boroughService.create(params);
+    if (!create.success)
+    {
+        fail("Creating borough failed.")
+    }
+    const borough = create.body
+
+    const updateParams = {
+        name: "testBorough2",
+    }
+    const update = await boroughService.update(borough.id, updateParams)
+    expect(update.success).toEqual(true)
+
+    const show = await boroughService
+        .show(borough.id)
+        .catch((err: Error) => fail("Could not find the borough by ID"))
+    const updatedBorough = show.borough
+    expect(updatedBorough.id).toEqual(borough.id)
+    expect(updatedBorough.name).toEqual("testBorough2")
+})
+
+test("update invalid borough", async () => {
+    expect.assertions(1);
+    const boroughService: BoroughService = new BoroughService();
+    const params = {
+        name: "testBorough",
+    }
+    await boroughService.update(1,params)
+    .then(() => fail())
+    .catch((err : Error) => expect(true).toEqual(true))
+})
+
+afterAll(async () => {
+    await database.close()
+})
