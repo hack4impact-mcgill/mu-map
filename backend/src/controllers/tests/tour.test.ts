6abcd3ee1f62b1dadb8de947b469d89527dcc15e
Tony <tonyou1999@hotmail.com>
2020-12-07 15:27:51 -0500

    feat: add tests for create & show functions of tour service & controller

--- backend/src/controllers/tests/tour.test.ts
+++ backend/src/controllers/tests/tour.test.ts
@@ -1,5 +1,122 @@
+import { EmptyResultError } from "sequelize";
 import { TourController } from "../tour.controller";
 
 jest.mock("../../services/tour.service");
 
-// TODO: Add tour controller tests
+const mockResponse = () => {
+  const res = {} as any;
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+const mockPOSTRequest = (tourName: String | undefined) => {
+  return {
+    body: { name: tourName },
+  } as any;
+};
+
+const mockGETRequest = (tourId: Number) => {
+  return {
+    params: { id: tourId },
+  } as any;
+};
+
+test("valid POST request", async () => {
+  expect.assertions(2);
+  const req = mockPOSTRequest("testtour");
+  const res = mockResponse();
+
+  const tourController: TourController = new TourController();
+  tourController.tourService.create = jest.fn().mockResolvedValue({
+    body: {
+      id: 1,
+      name: "testtour",
+    },
+    success: true,
+  });
+
+  await tourController.create(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(201);
+  expect(res.json).toHaveBeenCalledWith(
+    expect.objectContaining({
+      body: expect.objectContaining({
+        id: 1,
+        name: "testtour",
+      })
+    })
+  )
+});
+
+test("invalid POST request", async () => {
+  expect.assertions(1);
+  const req = mockPOSTRequest(undefined);
+  const res = mockResponse();
+
+  const tourController: TourController = new TourController();
+  tourController.tourService.create = jest.fn().mockImplementation(() => {
+    throw new Error();
+  });
+  await tourController.create(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(500);
+});
+
+test("valid GET request", async () => {
+  expect.assertions(2);
+  const req = mockGETRequest(1);
+  const res = mockResponse();
+
+  const tourController: TourController = new TourController();
+  tourController.tourService.show = jest.fn().mockResolvedValue({
+    tour: {
+      id: 1,
+      name: "testtour",
+    },
+    success: true,
+  });
+
+  await tourController.show(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(202);
+  expect(res.json).toHaveBeenCalledWith(
+    expect.objectContaining({
+      tour: expect.objectContaining({
+        id: 1,
+        name: "testtour",
+      }),
+    })
+  );
+});
+
+test("tour not found GET request", async () => {
+  expect.assertions(2);
+  const req = mockGETRequest(1);
+  const res = mockResponse();
+
+  const tourController: TourController = new TourController();
+  tourController.tourService.show = jest.fn().mockImplementation(() => {
+    throw new EmptyResultError();
+  });
+  await tourController.show(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(404);
+  expect(res.json).toHaveBeenCalledWith({
+    error: "Tour not found by id!",
+  });
+});
+
+test("invalid GET request", async () => {
+  expect.assertions(1);
+  const req = mockGETRequest(1);
+  const res = mockResponse();
+
+  const tourController: TourController = new TourController();
+  tourController.tourService.show = jest.fn().mockImplementation(() => {
+    throw new Error();
+  });
+  await tourController.show(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(500);
+});
--- backend/src/services/tests/tour.test.ts
+++ backend/src/services/tests/tour.test.ts
@@ -1,3 +1,52 @@
+import { database } from "../../config/database";
+import { Tour } from "../../models/tour.model";
 import { TourService } from "../tour.service";
 
-// TODO: Add tour service tests
+beforeEach(async () => {
+  await Tour.sync({ force: true });
+});
+
+test("create tour", async () => {
+  expect.assertions(3);
+  const tourService: TourService = new TourService();
+  const params = {
+    name: "testtour",
+    description: "testtour"
+  };
+  const create = await tourService.create(params, []);
+  expect(create.success).toEqual(true);
+  expect(create.body!.id).toEqual(1);
+  expect(create.body!.name).toEqual("testtour");
+});
+
+test("show valid tour", async () => {
+  expect.assertions(3);
+  const tourService: TourService = new TourService();
+  const params = {
+    name: "testtour",
+    description: "testtour"
+  };
+  const create = await tourService.create(params, []);
+  if (!create.success) {
+    fail("Creating tour failed.");
+  }
+  const tour = create.body!;
+
+  const show = await tourService.show(tour.id);
+  expect(show.success).toEqual(true);
+  expect(show.tour!.id).toEqual(tour.id);
+  expect(show.tour!.name).toEqual(tour.name);
+});
+
+test("show invalid tour", async () => {
+  expect.assertions(1);
+  const tourService: TourService = new TourService();
+  await tourService
+    .show(1)
+    .then(() => fail())
+    .catch((err: Error) => expect(true).toEqual(true));
+});
+
+afterAll(async () => {
+  await database.close();
+});
