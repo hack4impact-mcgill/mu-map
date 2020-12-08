8fc5963aa2eb4f40ea197269a4fd9cf2c06ac10f
Sibo Yang <59661782+SiboYang@users.noreply.github.com>
2020-12-08 15:59:09 +0800

    Feature/41 add test collection (#46)
    * finish controller tests
    
    * prettier
    
    * add test for service (exist bug about sequelize)
    
    * Fix controller test
    
    * prettier
    
    * added coordinate to mural in tests
    
    * remove redundant code
    
    Co-authored-by: Jacek Dziewonski <jacek@live.ca>

---
+++ backend/src/controllers/tests/muralcollection.test.ts
@@ -0,0 +1,178 @@
+import { database } from "../../config/database";
+import { EmptyResultError } from "sequelize";
+import { MuralCollectionController } from "../muralcollection.controller";
+
+jest.mock("../../services/muralcollection.service");
+
+const mockResponse = () => {
+  const res = {} as any;
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+const mockPOSTRequest = (
+  collectionName: String | undefined,
+  collectionDescription: String | undefined,
+  collectionMurals: number[]
+) => {
+  return {
+    body: {
+      collection: {
+        name: collectionName,
+        description: collectionDescription,
+      },
+      murals: collectionMurals,
+    },
+  } as any;
+};
+
+const mockGETRequest = (collectionId: Number) => {
+  return {
+    params: { id: collectionId },
+  } as any;
+};
+
+const mockPUTRequest = (collectionId: Number, collectionName: String) => {
+  return {
+    params: { id: collectionId },
+    body: { name: collectionName },
+  } as any;
+};
+
+test("valid POST request", async () => {
+  expect.assertions(2);
+  const req = mockPOSTRequest("testCollection", "des", [1, 2, 3]);
+  const res = mockResponse();
+
+  const collectionController: MuralCollectionController = new MuralCollectionController();
+  collectionController.collectionService.create = jest.fn().mockResolvedValue({
+    body: {
+      id: 1,
+      collection: {
+        name: "testCollectoin",
+        description: "des"
+      },
+      murals: [1, 2, 3],
+    },
+    success: true,
+  });
+
+  await collectionController.create(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(201);
+
+  expect(res.json).toHaveBeenCalledWith(
+    expect.objectContaining({
+      body: expect.objectContaining({
+        id: 1,
+        collection: {
+          name: "testCollectoin",
+          description: "des"
+        },
+        murals: [1, 2, 3],
+      }),
+    })
+  );
+});
+
+test("invalid POST request", async () => {
+  expect.assertions(1);
+  const req = mockPOSTRequest(undefined, "des", [1, 2, 3]);
+  const res = mockResponse();
+
+  const collectionController: MuralCollectionController = new MuralCollectionController();
+  collectionController.collectionService.create = jest
+    .fn()
+    .mockImplementation(() => {
+      throw new Error();
+    });
+  await collectionController.create(req, res);
+
+  expect(res.status).toHaveBeenLastCalledWith(500);
+});
+
+test("valid GET request", async () => {
+  expect.assertions(2);
+  const req = mockGETRequest(1);
+  const res = mockResponse();
+
+  const collectionController: MuralCollectionController = new MuralCollectionController();
+
+  collectionController.collectionService.show = jest.fn().mockResolvedValue({
+    collection: {
+      id: 1,
+      name: "testCollection",
+    },
+    success: true,
+  });
+
+  await collectionController.show(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(202);
+  expect(res.json).toHaveBeenCalledWith(
+    expect.objectContaining({
+      collection: expect.objectContaining({
+        id: 1,
+        name: "testCollection",
+      }),
+    })
+  );
+});
+
+test("collection not found in GET request", async () => {
+  expect.assertions(2);
+  const req = mockGETRequest(1);
+  const res = mockResponse();
+  const collectionController: MuralCollectionController = new MuralCollectionController();
+
+  collectionController.collectionService.show = jest
+    .fn()
+    .mockImplementation(() => {
+      throw new EmptyResultError();
+    });
+  await collectionController.show(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(404);
+  expect(res.json).toHaveBeenCalledWith({
+    error: "No collection found with this id",
+  });
+});
+
+test("valid PUT request", async () => {
+  expect.assertions(2);
+  const req = mockPUTRequest(1, "testCollection");
+  const res = mockResponse();
+
+  const collectionController: MuralCollectionController = new MuralCollectionController();
+  collectionController.collectionService.update = jest.fn().mockResolvedValue({
+    success: true,
+  });
+
+  await collectionController.update(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(202);
+  expect(res.json).toHaveBeenCalledWith({
+    data: "successfully updated",
+  });
+});
+
+test("collection not found PUT request", async () => {
+  expect.assertions(1);
+  const req = mockPUTRequest(1, "testCollection");
+  const res = mockResponse();
+
+  const collectionController: MuralCollectionController = new MuralCollectionController();
+  collectionController.collectionService.update = jest
+    .fn()
+    .mockImplementation(() => {
+      throw new Error();
+    });
+  await collectionController.update(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(500);
+});
+
+afterAll(async () => {
+  await database.close();
+});
---
+++ backend/src/services/tests/muralcollection.test.ts
@@ -0,0 +1,145 @@
+import { database } from "../../config/database";
+import { MuralCollection } from "../../models/muralcollection.model";
+import { MuralCollectionService } from "../muralcollection.service";
+import { Mural } from "../../models/mural.model";
+import { Borough } from "../../models/borough.model";
+import { Artist } from "../../models/artist.model";
+
+const params = {
+  name: "testCollection",
+  description: "des",
+};
+
+beforeAll(async () => {
+  Mural.belongsTo(Borough, {
+    foreignKey: { allowNull: false, name: "boroughId" },
+  });
+  Mural.belongsTo(Artist, {
+    foreignKey: { allowNull: false, name: "artistId" },
+  });
+  Mural.belongsToMany(MuralCollection, {
+    foreignKey: "muralId",
+    through: "murals_in_collection",
+    as: "collections",
+  });
+  MuralCollection.belongsToMany(Mural, {
+    foreignKey: "collectionId",
+    through: "murals_in_collection",
+    as: "murals",
+  });
+});
+
+beforeEach(async () => {
+  await MuralCollection.sync({ force: true });
+  const params = {
+    name: "testmural",
+    boroughId: "1",
+    artistId: "1",
+    year: 1234,
+    city: "montreal",
+    address: "1234 street",
+    partners: ["partner 1", "partner 2"],
+    coordinates: {
+      type: "Point",
+      coordinates: [-87.123123, 41.232454],
+    },
+  };
+  await Mural.create<Mural>(params);
+
+  const params2 = {
+    name: "testmural2",
+    boroughId: "1",
+    artistId: "1",
+    year: 1234,
+    city: "montreal",
+    address: "1234 street",
+    partners: ["partner 1", "partner 2"],
+    coordinates: {
+      type: "Point",
+      coordinates: [-87.123123, 41.232454],
+    },
+  };
+  await Mural.create<Mural>(params2);
+});
+
+test("create mural collection", async () => {
+  expect.assertions(4);
+  const collectionService: MuralCollectionService = new MuralCollectionService();
+
+  const murals: number[] = [1, 2];
+  const create = await collectionService.create(params, murals);
+  expect(create.success).toEqual(true);
+  expect(create.body.id).toEqual(1);
+  expect(create.body.name).toEqual("testCollection");
+  expect(create.body.description).toEqual("des");
+});
+
+test("show valid mural collection", async () => {
+  expect.assertions(4);
+  const collectionService: MuralCollectionService = new MuralCollectionService();
+
+  const murals: number[] = [1, 2];
+  const create = await collectionService.create(params, murals);
+  if (!create.success) {
+    fail("Creating mural collection failed.");
+  }
+
+  const show = await collectionService.show(create.body.id);
+  expect(show.success).toEqual(true);
+  expect(show.collection.id).toEqual(create.body.id);
+  expect(show.collection.name).toEqual(create.body.name);
+  expect(show.collection.description).toEqual(create.body.description);
+});
+
+test("show invalid collection", async () => {
+  expect.assertions(1);
+  const collectionService: MuralCollectionService = new MuralCollectionService();
+  await collectionService
+    .show(1)
+    .then(() => fail())
+    .catch((err: Error) => expect(true).toEqual(true));
+});
+
+test("update mural collection", async () => {
+  expect.assertions(4);
+  const collectionService: MuralCollectionService = new MuralCollectionService();
+
+  const murals: number[] = [1, 2];
+  const create = await collectionService.create(params, murals);
+  if (!create.success) {
+    fail("Creating mural collection failed.");
+  }
+  const collection = create.body;
+
+  const updateParams = {
+    name: "testCollection2",
+    description: "cri",
+  };
+  const update = await collectionService.update(collection.id, updateParams);
+  expect(update.success).toEqual(true);
+
+  const show = await collectionService
+    .show(collection.id)
+    .catch((err: Error) => {
+      fail("Could not find the mural collection by ID");
+    });
+
+  const updateCollection = show.collection;
+  expect(updateCollection.id).toEqual(collection.id);
+  expect(updateCollection.name).toEqual("testCollection2");
+  expect(updateCollection.description).toEqual("cri");
+});
+
+test("update invalid mural collection", async () => {
+  expect.assertions(1);
+  const collectionService: MuralCollectionService = new MuralCollectionService();
+
+  await collectionService
+    .update(1, params)
+    .then(() => fail())
+    .catch((err: Error) => expect(true).toEqual(true));
+});
+
+afterAll(async () => {
+  await database.close();
+});
