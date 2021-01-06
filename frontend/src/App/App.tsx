a6eaf0e21723fb438b2bd869ec3e1963168356b4
Jacek Dziewonski <46234488+JDziewonski98@users.noreply.github.com>
2021-01-06 08:04:41 -0800

    Feature/53 sidebar (#65)
    * cleaned up test setup procedure
    * added env variable for sql logs
    * added styles in ts
    * added general elements to sidebar
    * added + button to open sidebar

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Map from "../Map/Map";
+import Sidebar from "../sidebar/Sidebar";
 import PlusButton from "../plusButton/PlusButton";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const sidebarTitle = "Example Sidebar";
+
   return (
     <div className="App">
       <Map mapContainer={document.getElementById("root")} />
-      <PlusButton isVisible={true} />
+      <Sidebar
+        name={sidebarTitle}
+        isVisible={sidebarOpen}
+        closeSidebar={toggleSidebar}
+      />
+      <PlusButton isVisible={true} handleClick={toggleSidebar} />
     </div>
   );
 }
---
+++ frontend/src/sidebar/Sidebar.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import CloseIcon from "@material-ui/icons/Close";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      paper: "primary",
+    },
+    title: {
+      fontSize: "larger",
+      fontWeight: 500,
+      paddingLeft: theme.spacing(3),
+      paddingRight: theme.spacing(25),
+    },
+    closeIcon: {
+      cursor: "pointer",
+      float: "right",
+      marginTop: theme.spacing(2),
+      marginRight: theme.spacing(3),
+    },
+    flexContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    bottomButton: {
+      fontWeight: 500,
+    },
+    bottomButtonContainer: {
+      display: "flex",
+      justifyContent: "space-evenly",
+    },
+    deleteButtonInvisible: {
+      display: "none",
+    },
+  })
+);
+
+interface ISidebarProps {
+  name: string;
+  children?: React.ReactNode | null;
+  isVisible: boolean;
+  closeSidebar: () => void;
+}
+
+function Sidebar(props: ISidebarProps) {
+  const styles = useStyles();
+  const [deleteButtonVisible] = useState<boolean>(false);
+
+  return (
+    <div>
+      <Drawer
+        className={styles.drawer}
+        open={props.isVisible}
+        onClose={props.closeSidebar}
+      >
+        <div>
+          <CloseIcon
+            onClick={props.closeSidebar}
+            className={styles.closeIcon}
+          />
+        </div>
+
+        <p className={styles.title}>{props.name}</p>
+        <div className={styles.flexContainer}>{props.children}</div>
+
+        <div className={styles.bottomButtonContainer}>
+          <Button color="primary" size="small" className={styles.bottomButton}>
+            Cancel
+          </Button>
+
+          <Button color="primary" size="small">
+            Save
+          </Button>
+
+          <Button
+            className={deleteButtonVisible ? "" : styles.deleteButtonInvisible}
+            size="small"
+            color="secondary"
+          >
+            Delete
+          </Button>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
+
+export default Sidebar;
