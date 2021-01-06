082a9c64fa375053e163a711ea85542ce7d60364
Lin Xiao Zheng <43737599+zhenglinxiao@users.noreply.github.com>
2021-01-05 21:11:11 -0500

    Feature/60 plus button (#64)
    * started plus button
    * fix: isVisible prop
    * Update PlusButton.tsx

---
+++ frontend/src/plusButton/PlusButton.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import "./PlusButton.css";
+
+interface IPlusButtonProps {
+  isVisible: boolean;
+}
+
+function plusButtonClicked() {
+  // TODO: Notify App
+  console.log("Button Clicked"); // tmp line to check if the call works
+}
+
+function PlusButton(props: IPlusButtonProps) {
+  return (
+    <div className="button">
+      {props.isVisible ? (
+        <Fab
+          size="medium"
+          color="primary"
+          aria-label="add"
+          onClick={plusButtonClicked}
+        >
+          <AddIcon />
+        </Fab>
+      ) : null}
+    </div>
+  );
+}
+
+export default PlusButton;
