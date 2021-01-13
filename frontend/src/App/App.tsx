3fac7d9cdc95a2fb121ff2e19805962d9f690724
Ted Spare <ted@Teds-MacBook-Pro.local>
2021-01-13 14:51:57 -0500

    Change import path

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -4,7 +4,7 @@ import Map from "../Map/Map";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
 import Sidebar from "../sidebar/Sidebar";
 import PlusButton from "../plusButton/PlusButton";
-import MuralForm from "../MuralForm/MuralForm"
+import MuralForm from "../muralForm/MuralForm";
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
