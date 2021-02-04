a2bcabb7cac903b657c74eac2f36b5d0c7503891
tedspare <ted.spare@gmail.com>
2021-02-03 20:33:25 -0500

    Add POST integration. Add mural cards

--- frontend/src/CollectionForm/CollectionForm.tsx
+++ frontend/src/CollectionForm/CollectionForm.tsx
@@ -1,150 +1,158 @@
 import {
-    Button,
-    createStyles,
-    InputAdornment,
-    InputBase,
-    makeStyles,
-    TextField,
-    Theme
+  createStyles,
+  InputAdornment,
+  InputBase,
+  makeStyles,
+  TextField,
+  Theme
 } from "@material-ui/core";
 import axios from "axios";
 import React, { SyntheticEvent, useEffect, useState } from "react";
 import EditIcon from '@material-ui/icons/Edit';
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import ActionButtons from "../ActionButtons/ActionButtons";
+import SearchCard from "SideBarSearch/searchCard";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        flexContainer: {
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-            width: "40vw",
-            maxWidth: "500px",
-            padding: theme.spacing(3)
-        },
-        field: {
-            width: "100%",
-            margin: theme.spacing(2, 0, 2, 0)
-        },
-        title: {
-            fontSize: "180%",
-        },
-    })
+  createStyles({
+    flexContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "start",
+      width: "40vw",
+      maxWidth: "500px",
+      padding: theme.spacing(3)
+    },
+    field: {
+      width: "100%",
+      margin: theme.spacing(0, 0, 2, 0)
+    },
+    title: {
+      fontSize: "180%",
+    },
+  })
 );
 
 function CollectionForm() {
-    const [murals, setMurals] = useState<any>([]);
-    const [muralsInCollection, setMuralsInCollection] = useState<any>([]);
-    const [muralResults, setMuralResults] = useState<any>([]);
-    const [muralQuery, setMuralQuery] = useState<string>("");
+  const [murals, setMurals] = useState<any>([]);
+  const [muralsInCollection, setMuralsInCollection] = useState<any>([]);
+  const [muralResults, setMuralResults] = useState<any>([]);
+  const [muralQuery, setMuralQuery] = useState<string>("");
 
-    const [description, setDescription] = useState([]);
-    const [editingDesc, setEditingDesc] = useState<boolean>(false);
-    const [hoveringDesc, setHoveringDesc] = useState<boolean>(false);
+  const [description, setDescription] = useState([]);
+  const [editingDesc, setEditingDesc] = useState<boolean>(false);
+  const [hoveringDesc, setHoveringDesc] = useState<boolean>(false);
 
-    const [title, setTitle] = useState([]);
-    const [editingTitle, setEditingTitle] = useState<boolean>(false);
-    const [hoveringTitle, setHoveringTitle] = useState<boolean>(false);
+  const [title, setTitle] = useState([]);
+  const [editingTitle, setEditingTitle] = useState<boolean>(false);
+  const [hoveringTitle, setHoveringTitle] = useState<boolean>(false);
 
-    const styles = useStyles();
+  const styles = useStyles();
 
-    const handleAddMural = (addedMural: any) => {
-        if (!addedMural) return
-        if (muralsInCollection.some((currMural: any) => currMural.id === addedMural.id))
-            return
+  const handleAddMural = (addedMural: any) => {
+    if (!addedMural) return
+    if (muralsInCollection.some((currMural: any) => currMural.id === addedMural.id))
+      return
 
-        setMuralsInCollection((oldMurals: any) => [...oldMurals, addedMural]);
-    };
+    setMuralsInCollection((oldMurals: any) => [...oldMurals, addedMural]);
+  };
 
-    const handleSave = () => {
-        console.log(title)
-        console.log(description)
-        console.log(muralsInCollection)
-    };
+  const handleSave = () => {
+    // TODO: remember to replace this with localhost before merging
+    axios.post('http://mumapbackend.us-east-1.elasticbeanstalk.com/collection', {
+      collection: {
+        name: title,
+        description: description
+      },
+      murals: muralsInCollection.map((mural: any) => mural.id)
+    })
+      .then((response: any) => { console.log("Collection added!") })
+      .catch((err: any) => console.log(err));
+  };
 
-    const handleCancel = () => {
-        console.log("cancel")
-    };
+  const handleCancel = () => {
+    console.log("cancel")
+  };
 
-    useEffect(() => {
-        // TODO: remember to replace this with localhost before merging
-        axios.get('http://mumapbackend.us-east-1.elasticbeanstalk.com/mural')
-            .then((response) => {
-                if (response.data) setMurals(response.data.murals.rows)
-            })
-            .catch(err => console.log(err));
-    }, []);
+  useEffect(() => {
+    // TODO: remember to replace this with localhost before merging
+    axios.get('http://mumapbackend.us-east-1.elasticbeanstalk.com/mural')
+      .then((response) => {
+        if (response.data) setMurals(response.data.murals.rows)
+      })
+      .catch(err => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        const filtered = murals.filter((mural: any) => {
-            return mural.name.toLowerCase().includes(muralQuery)
-        });
-        setMuralResults(filtered);
-    }, [muralQuery, murals]);
+  useEffect(() => {
+    const filtered = murals.filter((mural: any) => {
+      return mural.name.toLowerCase().includes(muralQuery)
+    });
+    setMuralResults(filtered);
+  }, [muralQuery, murals]);
 
-    return (
-        <div>
-            <form
-                noValidate
-                autoComplete="off"
-                onSubmit={(e: SyntheticEvent) => { e.preventDefault(); handleSave() }}>
-                <div className={styles.flexContainer}>
-                    <InputBase
-                        className={`${styles.title} ${styles.field}`}
-                        defaultValue="Name the collection"
-                        onChange={(e: any) => setTitle(e.target.value)}
-                        inputProps={{ 'aria-label': 'New collection title' }}
-                        onClick={() => setEditingTitle(true)}
-                        onBlur={() => setEditingTitle(false)}
-                        onMouseEnter={() => setHoveringTitle(true)}
-                        onMouseLeave={() => setHoveringTitle(false)}
-                        endAdornment={!editingTitle && (
-                            <InputAdornment position="start">
-                                <EditIcon color={hoveringTitle ? "primary" : "action"} />
-                            </InputAdornment>
-                        )}
-                    />
-                    <InputBase
-                        className={styles.field}
-                        multiline
-                        rows={4}
-                        placeholder="Add a description"
-                        onChange={(e: any) => setDescription(e.target.value)}
-                        onClick={() => setEditingDesc(true)}
-                        onBlur={() => setEditingDesc(false)}
-                        onMouseEnter={() => setHoveringDesc(true)}
-                        onMouseLeave={() => setHoveringDesc(false)}
-                        endAdornment={!editingDesc && (
-                            <InputAdornment position="start">
-                                <EditIcon color={hoveringDesc ? "primary" : "action"} />
-                            </InputAdornment>
-                        )}
-                    />
-                    <div className={styles.field}>
-                        <Autocomplete
-                            freeSolo
-                            options={muralResults.map((mural: any) => mural)}
-                            getOptionLabel={(mural: any) => mural.name}
-                            onChange={(e: any, value: any) => handleAddMural(value)}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Add a mural"
-                                    variant="outlined"
-                                    size="small"
-                                    onChange={
-                                        (e: any) => setMuralQuery(e.target.value.toLowerCase())
-                                    }
-                                />
-                            )}
-                        />
-                    </div>
-                </div>
-            </form>
-            <ActionButtons saveCallback={handleSave} cancelCallback={handleCancel} />
+  return (
+    <div>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e: SyntheticEvent) => { e.preventDefault(); handleSave() }}>
+        <div className={styles.flexContainer}>
+          <InputBase
+            className={`${styles.title} ${styles.field}`}
+            defaultValue="Name the collection"
+            onChange={(e: any) => setTitle(e.target.value)}
+            inputProps={{ 'aria-label': 'New collection title' }}
+            onClick={() => setEditingTitle(true)}
+            onBlur={() => setEditingTitle(false)}
+            onMouseEnter={() => setHoveringTitle(true)}
+            onMouseLeave={() => setHoveringTitle(false)}
+            endAdornment={!editingTitle && (
+              <InputAdornment position="start">
+                <EditIcon color={hoveringTitle ? "primary" : "action"} />
+              </InputAdornment>
+            )}
+          />
+          <InputBase
+            className={styles.field}
+            multiline
+            rows={4}
+            placeholder="Add a description"
+            onChange={(e: any) => setDescription(e.target.value)}
+            onClick={() => setEditingDesc(true)}
+            onBlur={() => setEditingDesc(false)}
+            onMouseEnter={() => setHoveringDesc(true)}
+            onMouseLeave={() => setHoveringDesc(false)}
+            endAdornment={!editingDesc && (
+              <InputAdornment position="start">
+                <EditIcon color={hoveringDesc ? "primary" : "action"} />
+              </InputAdornment>
+            )}
+          />
+          <div className={styles.field}>
+            <Autocomplete
+              freeSolo
+              options={muralResults.map((mural: any) => mural)}
+              getOptionLabel={(mural: any) => mural.name}
+              onChange={(e: any, value: any) => handleAddMural(value)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Add a mural"
+                  variant="outlined"
+                  size="small"
+                  onChange={
+                    (e: any) => setMuralQuery(e.target.value.toLowerCase())
+                  }
+                />
+              )}
+            />
+          </div>
         </div>
-    )
+      </form>
+      <SearchCard searchCards={muralsInCollection} />
+      <ActionButtons saveCallback={handleSave} cancelCallback={handleCancel} />
+    </div>
+  )
 }
 
-export default CollectionForm
+export default CollectionForm;
--- frontend/src/SideBarSearch/searchCard.tsx
+++ frontend/src/SideBarSearch/searchCard.tsx
@@ -4,68 +4,69 @@ import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        flexContainer: {
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flexStart",
-        },
-        card: {
-            width: "450px",
-            margin: "5%",
-            display: "flex",
-        },
-        icon: {
-            width: '27%',
-            height: '100%',
-        },
-        properties: {
-            marginLeft: '5%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'spaceBetween',
-            width: '30%',
-        },
-        type: {
-            marginLeft: theme.spacing(9)
-        }
-    }),
+  createStyles({
+    flexContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flexStart",
+      width: "100%",
+      maxWidth: "450px"
+    },
+    card: {
+      margin: theme.spacing(0, 2, 2, 3),
+      display: "flex",
+      justifyContent: "flex-start"
+    },
+    icon: {
+      maxWidth: "25%",
+      height: "100%"
+    },
+    properties: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      padding: theme.spacing(0, 0, 1, 2),
+      width: "50%"
+    },
+    type: {
+      margin: theme.spacing(1, 0, 0, 1),
+      color: theme.palette.error.main
+    }
+  }),
 );
 
 interface ISearchCardProps {
-    searchCards: any;
+  searchCards: any;
 }
 
 function SearchCard(props: ISearchCardProps) {
-    const styles = useStyles();
+  const styles = useStyles();
 
-    return (
-        <div className={styles.flexContainer}>
-            {
-                props.searchCards.map((mural: any, index: any) => {
-
-                    return (<Card className={styles.card}>
-
-                        <img className={styles.icon}
-                            src="https://cdn2.lamag.com/wp-content/uploads/sites/6/2020/01/kobe-mural-mr-brainwash-chris-delmas-afp-getty-1068x712.jpg"
-                            alt="mural"></img>
-                        <div className={styles.properties}>
-                            <Typography variant="subtitle1">
-                                {mural.name}
-                            </Typography>
-                            <Typography variant="body2">
-                                {mural.address}, {mural.city}
-                            </Typography>
-
-                        </div>
-                        <Typography variant="body1" className={styles.type}>
-                            mural
-                        </Typography>
-                    </Card>)
-                })
-            }
-        </div>
-    );
+  return (
+    <div className={styles.flexContainer}>
+      {props.searchCards.map((mural: any, index: any) => {
+        return (
+          <Card className={styles.card} key={mural.id}>
+            <img className={styles.icon}
+              src="https://cdn2.lamag.com/wp-content/uploads/sites/6/2020/01/kobe-mural-mr-brainwash-chris-delmas-afp-getty-1068x712.jpg"
+              alt="mural"></img>
+            <div className={styles.properties}>
+              <Typography variant="subtitle1">
+                {mural.name}
+              </Typography>
+              <Typography variant="body2">
+                {mural.address}, {mural.city}
+              </Typography>
+            </div>
+            <Typography variant="body1" className={styles.type}>
+              mural
+            </Typography>
+          </Card>
+        )
+      })
+      }
+    </div>
+  );
 }
 
 export default SearchCard;
