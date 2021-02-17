e5f305ed1961aa06ab6e2c40e43f93c0d5ab30ef
tedspare <ted.spare@gmail.com>
2021-02-16 23:02:52 -0500

    Update map styling. Add zoom controls

--- frontend/src/Map/Map.tsx
+++ frontend/src/Map/Map.tsx
@@ -4,10 +4,23 @@ import {
   DEFAULT_LONGITUDE,
   DEFAULT_LATITUDE,
   DEFAULT_ZOOM,
+  MAPBOX_STYLE_URL
 } from "constants/constants";
-import "./Map.css";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN || "";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      position: "absolute",
+      left: 0,
+      top: 0,
+      bottom: 0,
+      right: 0
+    }
+  })
+);
 
 interface IMapProps {
   mapContainer: HTMLElement | string | null;
@@ -15,20 +28,23 @@ interface IMapProps {
 }
 
 function Map({ mapContainer, murals }: IMapProps) {
+
+  const styles = useStyles();
+
   const [lng, setLng] = useState(DEFAULT_LONGITUDE);
   const [lat, setLat] = useState(DEFAULT_LATITUDE);
   const [zoom, setZoom] = useState(DEFAULT_ZOOM);
 
-
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: (mapContainer as any),
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: MAPBOX_STYLE_URL,
       center: [lng, lat],
       zoom: zoom,
       attributionControl: false
     });
 
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
     map.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
@@ -38,7 +54,6 @@ function Map({ mapContainer, murals }: IMapProps) {
       }),
       'bottom-right'
     );
-
     map.addControl(new mapboxgl.AttributionControl(), 'top-left');
 
     map.on("move", () => {
@@ -48,7 +63,7 @@ function Map({ mapContainer, murals }: IMapProps) {
     });
 
     murals.forEach((mural: any) => {
-      new mapboxgl.Marker()
+      new mapboxgl.Marker({ color: "#FC323E" })
         .setLngLat([
           mural.coordinates.coordinates[0],
           mural.coordinates.coordinates[1],
@@ -59,9 +74,7 @@ function Map({ mapContainer, murals }: IMapProps) {
   }, [murals]);
 
   return (
-    <div>
-      <div ref={(el) => (mapContainer = el)} className="mapContainer" />
-    </div>
+    <div ref={(el) => (mapContainer = el)} className={styles.container} />
   );
 }
 
