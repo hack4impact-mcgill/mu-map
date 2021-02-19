5ac71f1003f9971844e4c979b195a9eb616c4dbc
tedspare <ted.spare@gmail.com>
2021-02-19 11:29:55 -0500

    Update webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,6 +3,7 @@ import { InjectManifest } from 'workbox-webpack-plugin';
 module.exports = {
   plugins: [
     new InjectManifest({
+      swSrc: './src/service-worker.ts',
       maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
     })
   ]
