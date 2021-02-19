d79add289cec096747237d031fac96bd130eb594
tedspare <ted.spare@gmail.com>
2021-02-19 11:45:06 -0500

    Update webpack without SW

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,3 @@
-import { GenerateSW } from 'workbox-webpack-plugin';
-
 module.exports = {
-  plugins: [
-    new GenerateSW({
-      swSrc: './src/service-worker.ts',
-      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
-    })
-  ]
+  maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
 };
