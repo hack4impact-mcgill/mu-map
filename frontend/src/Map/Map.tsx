453a1698f3954811604142085abe816e5000d5a5
tedspare <ted.spare@gmail.com>
2021-02-20 16:16:15 -0500

    Remove unused eslint comment

--- frontend/src/Map/Map.tsx
+++ frontend/src/Map/Map.tsx
@@ -11,7 +11,6 @@ import {
 } from "constants/constants";
 import "./Map.css";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import mapboxgl from "mapbox-gl";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 (mapboxgl as any).workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
