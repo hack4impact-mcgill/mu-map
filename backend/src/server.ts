7e850cd232a6fe69ac59d869a0c9b33546d54a4f
Ted Spare <ted.spare@mail.mcgill.ca>
2021-03-30 01:42:59 -0400

    Revert "Feat/127 HTTPS server configuration (#150)"
    This reverts commit d5321e16146a12531ee045ed12624f5ba7d3795a.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,4 @@
 import app from "./app";
-import https from "https";
-import fs from "fs";
-
 const PORT = process.env.PORT || 3000;
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
