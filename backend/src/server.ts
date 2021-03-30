d5321e16146a12531ee045ed12624f5ba7d3795a
Tony <36376980+Tony9984@users.noreply.github.com>
2021-03-30 01:38:56 -0400

    Feat/127 HTTPS server configuration (#150)
    * feat: add https configuration to server
    * Remove faulty .ebextensions file for now

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,11 @@
 import app from "./app";
+import https from "https";
+import fs from "fs";
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
